85c3c99474f305837c616317597c1d8919113701
Vitor Martini <vitor-martini@outlook.com>
2022-08-29 17:58:51 -0300

    feat: add Produtos services

---
+++ src/produtos.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { Produto } from "./produto.model";
+
+@Injectable()
+export class ProdutosService {
+   constructor(
+    @InjectModel(Produto)
+    private produtoModel: typeof Produto   
+   ) { }
+
+    async obterTodos(): Promise<Produto[]> {
+        return this.produtoModel.findAll();
+    }
+
+    async obterUm(id: number): Promise<Produto> {
+        return this.produtoModel.findByPk(id);
+    }
+
+    async criar(produto: Produto) {
+        this.produtoModel.create(produto);
+    }
+
+    async alterar(produto: Produto): Promise<[number]> {
+        return this.produtoModel.update(produto, {
+            where: {
+                id: produto.id
+            }
+        });
+    }
+
+    async apagar(id: number) {
+        const produto: Produto = await this.obterUm(id);
+        if (produto) produto.destroy();
+    }
+
+}
