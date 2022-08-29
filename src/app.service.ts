7bb1d8ab58442a4efa93fdc02e74d578713ce6b3
Vitor Martini <vitor-martini@outlook.com>
2022-08-29 17:57:47 -0300

    feat: add sincronizaBancoDados method

---
+++ src/app.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { Sequelize } from 'sequelize-typescript';
+
+@Injectable()
+export class AppService {
+  constructor(private sequelize: Sequelize) { 
+    this.sincronizaBancoDados();
+  }
+
+  async sincronizaBancoDados() {
+    await this.sequelize.sync();
+  }
+}
